98d677a722016299b019b6aa28eaea5323e2b977
Dawid Przydryga <shivva90@gmail.com>
2020-05-19 11:37:32 +0200

    Add props share components between each other, and add colums component

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,12 +1,32 @@
 import React from 'react';
 import styles from './List.scss';
-import Hero from '../Hero/Hero.js'
+import Hero from '../Hero/Hero.js';
+import PropTypes from 'prop-types';
+import Column from '../Column/Column.js'
 
 class List extends React.Component {
+    static propTypes = {
+        title: PropTypes.node.isRequired,
+        children: PropTypes.node,
+    }
+    static defaultProps = {
+        children: <p>I can do all the things!!</p>
+    }
     render() {
         return (
             <section className={styles.component}>
-                <Hero />
+                <Hero titleText={this.props.title}
+                    image={this.props.image} />
+
+                <div className={styles.description}>
+                    {this.props.children}
+                </div>
+
+                <div className={styles.columns}>
+                    <Column title={'One'} />
+                    <Column title={'Two'} />
+                    <Column title={'Three'} />
+                </div>
             </section>
         )
     }
