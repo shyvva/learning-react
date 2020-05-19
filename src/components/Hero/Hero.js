17fd544d3892cf7528276343ba3642b7d8c0bd65
Dawid Przydryga <shivva90@gmail.com>
2020-05-19 10:08:00 +0200

    Add List to App component

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -4,7 +4,7 @@ import styles from './Hero.scss';
 const HeroComponent = () => (
     <header className={styles.component}>
         <h2 className={styles.title}>Things to do</h2>
-        <img src="http://uploads.kodilla.com/bootcamp/fer/11.react/space.png" className={styles.image}></img>
+        <img  className={styles.image} src="http://uploads.kodilla.com/bootcamp/fer/11.react/space.png"></img>
     </header>
 );
 
--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import styles from './List.scss';
 import Hero from '../Hero/Hero.js'
+
 class List extends React.Component {
     render() {
         return (
